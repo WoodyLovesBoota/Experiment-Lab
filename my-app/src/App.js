526e5d384f43244125983e39bb55dbede9afcf9d
Woody <woodylovesboota@gmail.com>
2023-08-18 08:35:22 +0900

    add counter example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,61 @@
+// import { useState } from "react";
+
+// const Button = ({ text, onClick }) => {
+//   return <button onClick={onClick}>{text} counter</button>;
+// };
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+
+//   const increaseCounter = () => {
+//     setCounter((prev) => prev + 1);
+//   };
+
+//   const decreaseCounter = () => {
+//     setCounter((prev) => prev - 1);
+//   };
+
+//   return (
+//     <div>
+//       <h2>Total clicks: {counter}</h2>
+//       <Button text="increase" onClick={increaseCounter} />
+//       <Button text="decrease" onClick={decreaseCounter} />
+
+//       <button
+//         onClick={() => {
+//           setCounter((prev) => prev + 1);
+//         }}
+//       >
+//         click me
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useState } from "react";
 
-const Button = ({ text, onClick }) => {
-  return <button onClick={onClick}>{text} counter</button>;
+const Button = (props) => {
+  return <button onClick={props.func}>{props.name}</button>;
 };
 
 function App() {
   const [counter, setCounter] = useState(0);
 
   const increaseCounter = () => {
-    setCounter((prev) => prev + 1);
+    setCounter(counter + 1);
   };
 
   const decreaseCounter = () => {
-    setCounter((prev) => prev - 1);
+    setCounter(counter - 1);
   };
 
   return (
     <div>
       <h2>Total clicks: {counter}</h2>
-      <Button text="increase" onClick={increaseCounter} />
-      <Button text="decrease" onClick={decreaseCounter} />
-
-      <button
-        onClick={() => {
-          setCounter((prev) => prev + 1);
-        }}
-      >
-        click me
-      </button>
+      <Button name="increase" func={increaseCounter} />
+      <Button name="decrease" func={decreaseCounter} />
     </div>
   );
 }
