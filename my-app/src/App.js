55b0c54ff902e5f54aed0552ef887d27d6b71514
woody <130626781+WoodyLovesBoota@users.noreply.github.com>
2023-08-19 18:16:51 +0900

    add state for useEffect experiment setting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
 
   useEffect(()=>{
     console.log("a");
-  })
+  },[age]);
 
   return <div></div>;
 }
