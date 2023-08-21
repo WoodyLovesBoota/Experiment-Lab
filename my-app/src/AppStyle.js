c0275217a485ae63bb0fa6986343ffd4634aa247
woody <woodylovesboota@gmail.com>
2023-08-21 21:17:55 +0900

    add useEffect test code

---
+++ my-app/src/AppStyle.js
@@ -0,0 +1,53 @@
+import styled, { keyframes } from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const rotationAni = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0;
+  }
+  50% {
+    border-radius:50%;
+
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${rotationAni} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${Emoji} {
+    &:hover {
+      font-size: 150px;
+    }
+  }
+`;
+
+function App() {
+  return (
+    <Father>
+      <Box>
+        <Emoji>a</Emoji>
+      </Box>
+    </Father>
+  );
+}
+
+export default App;
