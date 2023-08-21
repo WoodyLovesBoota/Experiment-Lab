88da50243c180b076a32d1505d3192633c26201f
woody <woodylovesboota@gmail.com>
2023-08-21 09:26:41 +0900

    add styled component example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,52 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const rotationAni = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0;
+  }
+  50% {
+    border-radius:50%;
+
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0;
+
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${rotationAni} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${Emoji} {
+    &:hover {
+      font-size: 150px;
+    }
+  }
+`;
 
 function App() {
-  return <div></div>;
+  return (
+    <Father>
+      <Box>
+        <Emoji>a</Emoji>
+      </Box>
+    </Father>
+  );
 }
 
 export default App;
